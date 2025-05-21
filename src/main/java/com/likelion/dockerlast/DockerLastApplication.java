29581591dd47e08e4bebca74751770b70c154748
shinchaerin79 <shinchaerin79@gmail.com>
2025-05-21 19:07:29 +0900

    ci/cd test

---
+++ src/main/java/com/likelion/dockerlast/DockerLastApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.dockerlast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerLastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerLastApplication.class, args);
+	}
+
+}
